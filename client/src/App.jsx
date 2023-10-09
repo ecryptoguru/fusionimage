07fc465419e3456c58d591f51cc955d1320e4e4d
Ankit Das <ecryptoguru@gmail.com>
2023-10-10 02:37:20 +0530

    Update1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,11 +8,16 @@ const App = () => (
   <BrowserRouter>
     <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
       <Link to="/">
-        <img src={logo} alt="logo" className="w-28 object-contain" />
+        <img 
+         src={logo}
+         alt="logo" 
+         className="w-16 object-contain" />
       </Link>
+    
 
       <Link to="/create-post" className="font-inter text-white bg-gradient-to-r from-purple-500 to-pink-500 
-       hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Create</Link>
+       hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 
+       font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Create</Link>
     </header>
     <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
       <Routes>
--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,9 +1,11 @@
 import download from './download.png';
 import logo from './logo.png';
 import preview from './preview.png';
+import logo2 from './logo.svg';
 
 export {
   download,
   logo,
   preview,
+  logo2,
 };
