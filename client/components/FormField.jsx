d17d50b35aa1de002771c64ae4c422819346c53d
Ankit Das <ecryptoguru@gmail.com>
2023-10-10 17:06:20 +0530

    Update2

--- client/components/FormField.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const FormField = () => {
-  return (
-    <div>FormField</div>
-  )
-}
-
-export default FormField
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,8 @@ const App = () => (
 
       <Link to="/create-post" className="font-inter text-white bg-gradient-to-r from-purple-500 to-pink-500 
        hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 
-       font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Create</Link>
+       font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+       Create</Link>
     </header>
     <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
       <Routes>
