2d5b9b84504be31638b9f86b9f00e849cc233759
Ankit Das <ecryptoguru@gmail.com>
2023-10-16 20:43:28 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,11 +11,13 @@ const App = () => (
         <img 
          src={logo}
          alt="logo" 
-         className="w-14 object-contain" />  
+         className="w-14 object-contain" />
       </Link>
-      <p className="text-black font-semibold text-lg ml-1 mt-3">FusionImage</p>
-      </div> 
-      <div className="flex justify-end gap-2 mt-1">
+      <Link to="/">
+       <p className="text-black font-semibold text-lg ml-1 mt-3">FusionImage</p>
+      </Link>
+    </div> 
+    <div className="flex justify-end gap-2 mt-1">
       <Link to="https://www.fusionwaveai.com" 
        target="_blank"  // Add this line to open the link in a new page
        className="text-white object-right bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l
@@ -27,7 +29,7 @@ const App = () => (
        focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
        Create AI Image
        </Link>
-       </div>
+     </div>
     </header>
     <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
       <Routes>
