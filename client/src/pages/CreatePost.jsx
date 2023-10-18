3321cfeaa9b0a7c493bd111b698d3eae02cd070f
Ankit Das <ecryptoguru@gmail.com>
2023-10-19 00:07:23 +0530

    Update

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -122,6 +122,7 @@ const CreatePost = () => {
                 className="w-full h-full object-contain"
               />
             ) : (
+              
               <img
                 src={preview}
                 alt="preview"
@@ -142,10 +143,26 @@ const CreatePost = () => {
             type="button"
             onClick={generateImage}
             className=" text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br 
-            focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+            focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm sm:px-5 px-3 sm:py-2.5 py-2 text-center mr-2 mb-2"
           >
             {generatingImg ? 'Generating...' : 'Generate'}
           </button>
+          <button 
+                type="button" 
+                onClick={() => {}}
+                className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br 
+                focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm sm:px-5 px-3 sm:py-2.5 py-2 text-center mr-2 mb-2"
+            >
+            Download
+           </button>
+           <button 
+                type="button" 
+                onClick={() => window.open('https://www.fusionwaveai.com/create-nft/')}
+                className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4
+                focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm sm:px-5 px-3 sm:py-2.5 py-2 text-center mr-2 mb-2"
+            >
+            Mint NFT
+           </button>
         </div>
 
         <div className="mt-10">
@@ -155,7 +172,7 @@ const CreatePost = () => {
           <button
             type="submit"
             className="mt-3 text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4
-            focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+            focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm sm:px-5 px-3 sm:py-2.5 py-2 text-center mr-2 mb-2"
           >
             {loading ? 'Sharing...' : 'Share with the Community'}
           </button>
