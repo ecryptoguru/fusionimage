59b19219446f5869c2f1b9ab7d8e3f670c1c3cbe
Ankit Das <ecryptoguru@gmail.com>
2023-10-20 00:16:47 +0530

    update

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -155,14 +155,6 @@ const CreatePost = () => {
             >
             Download
            </button>*/}
-           <button 
-                type="button" 
-                onClick={() => window.open('https://www.fusionwaveai.com/create-nft/')}
-                className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4
-                focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm sm:px-5 px-3 sm:py-2.5 py-2 text-center mr-2 mb-2"
-            >
-            Mint NFT
-           </button>
         </div>
 
         <div className="mt-10">
