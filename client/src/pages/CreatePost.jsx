9128a1bdcd89e8413115328262918354d58df486
Ankit Das <ecryptoguru@gmail.com>
2023-10-19 22:02:12 +0530

    update

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -147,14 +147,14 @@ const CreatePost = () => {
           >
             {generatingImg ? 'Generating...' : 'Generate'}
           </button>
-          <button 
+          {/*<button 
                 type="button" 
                 onClick={() => {}}
                 className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br 
                 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm sm:px-5 px-3 sm:py-2.5 py-2 text-center mr-2 mb-2"
             >
             Download
-           </button>
+           </button>*/}
            <button 
                 type="button" 
                 onClick={() => window.open('https://www.fusionwaveai.com/create-nft/')}
